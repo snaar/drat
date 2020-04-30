6a77027964e59216b542050e67e703a3f0573b07
Snaar <snaar@snaar.net>
2020-04-29 23:52:31 -0400

    clean up

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
-extern crate chopper_lib;
-use chopper_lib::chopper_cli::chopper_cli;
-use chopper_lib::error;
+use chopper_lib::{chopper_cli::chopper_cli, error};
 
 fn main() {
     error::handle_drive_error(chopper_cli(None, None, None));
